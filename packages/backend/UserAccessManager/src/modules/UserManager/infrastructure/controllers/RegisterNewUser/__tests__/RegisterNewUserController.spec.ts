8dab7a4616434e7a44f472d08077afd752280015
Pedro Henrique <sandbox0@hotmail.com>
2021-02-26 03:46:03 -0300

    test: add response object validations on 'RegisterNewUserController'

--- packages/backend/UserAccessManager/src/modules/UserManager/infrastructure/controllers/RegisterNewUser/__tests__/RegisterNewUserController.spec.ts
+++ packages/backend/UserAccessManager/src/modules/UserManager/infrastructure/controllers/RegisterNewUser/__tests__/RegisterNewUserController.spec.ts
@@ -1,20 +1,55 @@
 import RegisterNewUserController from '../RegisterNewUserController';
 
+import faker from 'faker';
+
 describe('Module - UserManager', () => {
   describe('RegisterNewUserController', () => {
-    // Validate: Full Name, Email and Password.
-
     it('should return HTTP Status Code 400 (Bad Request) if `fullName` is not provided', () => {
       const SUT = new RegisterNewUserController();
 
       const requestObject = {
-        email: '',
-        password: '',
+        fullName: undefined,
+        email: faker.internet.email(),
+        password: faker.internet.password(),
       };
 
-      const { statusCode } = SUT.execute(requestObject);
+      const { statusCode, error } = SUT.execute(requestObject);
 
       expect(statusCode).toBe(400);
+      expect(error?.type).toBe('InvalidRequestError');
+      expect(error?.message).toBe('[fullName] cannot be empty');
+    });
+
+    it('should return HTTP Status Code 400 (Bad Request) if `email` is not provided', () => {
+      const SUT = new RegisterNewUserController();
+
+      const requestObject = {
+        fullName: `${faker.name.firstName} ${faker.name.lastName}`,
+        email: undefined,
+        password: faker.internet.password(),
+      };
+
+      const { statusCode, error } = SUT.execute(requestObject);
+
+      expect(statusCode).toBe(400);
+      expect(error?.type).toBe('InvalidRequestError');
+      expect(error?.message).toBe('[email] cannot be empty');
+    });
+
+    it('should return HTTP Status Code 400 (Bad Request) if `password` is not provided', () => {
+      const SUT = new RegisterNewUserController();
+
+      const requestObject = {
+        fullName: `${faker.name.firstName()} ${faker.name.lastName()}`,
+        email: faker.internet.email(),
+        password: undefined,
+      };
+
+      const { statusCode, error } = SUT.execute(requestObject);
+
+      expect(statusCode).toBe(400);
+      expect(error?.type).toBe('InvalidRequestError');
+      expect(error?.message).toBe('[password] cannot be empty');
     });
   });
 });
