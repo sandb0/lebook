7991a4aba4d24c560e05fd151f0266118f1a2427
Pedro Henrique <sandbox0@hotmail.com>
2021-02-27 13:59:57 -0300

    test: change 'RegisterNewUserController' error messages

--- packages/backend/UserAccessManager/src/modules/UserManager/infrastructure/controllers/RegisterNewUser/__tests__/RegisterNewUserController.spec.ts
+++ packages/backend/UserAccessManager/src/modules/UserManager/infrastructure/controllers/RegisterNewUser/__tests__/RegisterNewUserController.spec.ts
@@ -4,6 +4,28 @@ import faker from 'faker';
 
 describe('Module - UserManager', () => {
   describe('RegisterNewUserController', () => {
+    it('should return HTTP Status Code 400 (Bad Request) if `fullName`, `email` or `password` is not provided', () => {
+      const SUT = new RegisterNewUserController();
+
+      const requestObject = {
+        fullName: undefined,
+        email: undefined,
+        password: undefined,
+      };
+
+      const { statusCode, error } = SUT.execute(requestObject);
+
+      const errorMessage = Array<string>(
+        '[fullName] cannot be empty',
+        '[email] cannot be empty',
+        '[password] cannot be empty'
+      );
+
+      expect(statusCode).toBe(400);
+      expect(error?.type).toBe('InvalidRequestError');
+      expect(error?.message).toEqual(errorMessage);
+    });
+
     it('should return HTTP Status Code 400 (Bad Request) if `fullName` is not provided', () => {
       const SUT = new RegisterNewUserController();
 
@@ -17,7 +39,7 @@ describe('Module - UserManager', () => {
 
       expect(statusCode).toBe(400);
       expect(error?.type).toBe('InvalidRequestError');
-      expect(error?.message).toBe('[fullName] cannot be empty');
+      expect(error?.message[0]).toBe('[fullName] cannot be empty');
     });
 
     it('should return HTTP Status Code 400 (Bad Request) if `email` is not provided', () => {
@@ -33,7 +55,7 @@ describe('Module - UserManager', () => {
 
       expect(statusCode).toBe(400);
       expect(error?.type).toBe('InvalidRequestError');
-      expect(error?.message).toBe('[email] cannot be empty');
+      expect(error?.message[0]).toBe('[email] cannot be empty');
     });
 
     it('should return HTTP Status Code 400 (Bad Request) if `password` is not provided', () => {
@@ -49,7 +71,7 @@ describe('Module - UserManager', () => {
 
       expect(statusCode).toBe(400);
       expect(error?.type).toBe('InvalidRequestError');
-      expect(error?.message).toBe('[password] cannot be empty');
+      expect(error?.message[0]).toBe('[password] cannot be empty');
     });
   });
 });
