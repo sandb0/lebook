b5b15f388a90ba0f63817507f0bdc4279039be0f
Pedro Henrique <sandbox0@hotmail.com>
2021-02-27 22:08:36 -0300

    test: add email validation in Email Value Object

--- packages/backend/UserAccessManager/src/modules/UserManager/infrastructure/controllers/RegisterNewUser/domain/valueObjects/Email/__tests__/Email.spec.ts
+++ packages/backend/UserAccessManager/src/modules/UserManager/infrastructure/controllers/RegisterNewUser/domain/valueObjects/Email/__tests__/Email.spec.ts
@@ -2,8 +2,12 @@ import EmailFactory from '../Email';
 
 describe('Module: UserManager', () => {
   describe('Email Value Object', () => {
-    it('should return an error if is `email` is empty', () => {
-      const emailValidatorMock = jest.fn();
+    it('should return an error message if `email` is empty', () => {
+      const EmailValidatorMock = jest.fn(() => ({
+        isEmail: jest.fn().mockReturnValue(false),
+      }));
+
+      const emailValidatorMock = new EmailValidatorMock();
 
       const SUT = new EmailFactory({
         emailValidator: emailValidatorMock,
@@ -12,6 +16,25 @@ describe('Module: UserManager', () => {
       const emailOrError = SUT.build({ email: '' });
 
       expect(emailOrError.error).toBe('[email] cannot be empty');
+    });
+
+    it('should return an error message if `email` is invalid', () => {
+      const EmailValidatorMock = jest.fn(() => ({
+        isEmail: jest.fn().mockReturnValue(false),
+      }));
+
+      const emailValidatorMock = new EmailValidatorMock();
+
+      const SUT = new EmailFactory({
+        emailValidator: emailValidatorMock,
+      });
+
+      const email = 'INVALID';
+      const emailOrError = SUT.build({ email });
+
+      expect(emailOrError.error).toBe(`[email] "${email}" is invalid`);
+      expect(emailValidatorMock.isEmail).toBeCalledTimes(1);
+      expect(emailValidatorMock.isEmail).toBeCalledWith(email);
     });
   });
 });
