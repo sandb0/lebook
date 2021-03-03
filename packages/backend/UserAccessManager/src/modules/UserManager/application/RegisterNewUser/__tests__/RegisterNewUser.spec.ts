628cee4b9d65bc90fdf28058066834eb49b60b47
Pedro Henrique <sandbox0@hotmail.com>
2021-03-03 00:51:20 -0300

    test: simplify the creation of new Value Objects and separate yourself factories

--- packages/backend/UserAccessManager/src/modules/UserManager/application/RegisterNewUser/__tests__/RegisterNewUser.spec.ts
+++ packages/backend/UserAccessManager/src/modules/UserManager/application/RegisterNewUser/__tests__/RegisterNewUser.spec.ts
@@ -1,18 +1,14 @@
-import EmailFactory from '../../../domain/valueObjects/Email/Email';
+import EmailFactory from '../../../domain/valueObjects/Email/EmailFactory';
 import RegisterNewUser, { RegisterNewUserProps } from '../RegisterNewUser';
 
-/**
- * - Chamar os Value Objects.
- * - Retornar um erro em cada de error dos ValuesObjects.
- */
 describe('Module - UserManager', () => {
   describe('RegisterNewUser', () => {
     it('should call EmailFactory with expected email value', () => {
       const EmailFactoryMock = jest.fn<Partial<EmailFactory>, []>();
       const emailFactoryMock = new EmailFactoryMock() as EmailFactory;
-      const ValueObjectCreation = jest.fn();
-
-      emailFactoryMock.build = jest.fn(() => new ValueObjectCreation());
+      emailFactoryMock.create = jest.fn().mockReturnValue({
+        error: '',
+      });
 
       const props: RegisterNewUserProps = {
         fullName: '',
@@ -22,15 +18,15 @@ describe('Module - UserManager', () => {
 
       new RegisterNewUser(emailFactoryMock).execute(props);
 
-      expect(emailFactoryMock.build).toBeCalledTimes(1);
-      expect(emailFactoryMock.build).toBeCalledWith({ email: props.email });
+      expect(emailFactoryMock.create).toBeCalledTimes(1);
+      expect(emailFactoryMock.create).toBeCalledWith({ email: props.email });
     });
   });
 
   it('should return an error if EmailFactory fail on creation', () => {
     const EmailFactoryMock = jest.fn<Partial<EmailFactory>, []>();
     const emailFactoryMock = new EmailFactoryMock() as EmailFactory;
-    emailFactoryMock.build = jest.fn().mockReturnValue({
+    emailFactoryMock.create = jest.fn().mockReturnValue({
       error: '[email] cannot be empty',
     });
 
--- packages/backend/UserAccessManager/src/modules/UserManager/domain/valueObjects/Email/__tests__/Email.spec.ts
+++ packages/backend/UserAccessManager/src/modules/UserManager/domain/valueObjects/Email/__tests__/Email.spec.ts
@@ -1,35 +1,41 @@
-import EmailFactory, { IEmailValidator } from '../Email';
+import { IEmailValidator } from '../Email';
+import EmailFactory from '../EmailFactory';
 
 describe('Module: UserManager', () => {
   describe('Email Value Object', () => {
-    it('should return an error message if `email` is empty', () => {
+    it('should set an error message if `email` is empty', () => {
       const EmailValidatorMock = jest.fn<Partial<IEmailValidator>, []>();
       const emailValidatorMock = new EmailValidatorMock() as IEmailValidator;
-      emailValidatorMock.isEmail = jest.fn();
+
+      const emailFactory = new EmailFactory({
+        emailValidator: emailValidatorMock,
+      });
 
       const email = undefined || '';
 
-      const SUT = new EmailFactory({
-        emailValidator: emailValidatorMock,
-      }).build({ email });
+      const emailObject = emailFactory.create({ email });
 
-      expect(SUT.error).toBe('[email] cannot be empty');
+      expect(emailObject.error).toBe('[email] cannot be empty');
     });
 
     it('should return an error message if `email` is invalid', () => {
       const EmailValidatorMock = jest.fn<Partial<IEmailValidator>, []>();
       const emailValidatorMock = new EmailValidatorMock() as IEmailValidator;
-      emailValidatorMock.isEmail = jest.fn(() => false);
+      emailValidatorMock.isEmail = jest.fn().mockReturnValue(false);
+
+      const emailFactory = new EmailFactory({
+        emailValidator: emailValidatorMock,
+      });
 
       const email = 'INVALID';
 
-      const SUT = new EmailFactory({
-        emailValidator: emailValidatorMock,
-      }).build({ email });
+      const emailObject = emailFactory.create({ email });
 
-      expect(SUT.error).toBe(`[email] "${email}" must be a valid email`);
       expect(emailValidatorMock.isEmail).toBeCalledTimes(1);
       expect(emailValidatorMock.isEmail).toBeCalledWith(email);
+      expect(emailObject.error).toBe(
+        `[email] "${email}" must be a valid email`
+      );
     });
   });
 });
